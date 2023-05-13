253626230df49755bb23553e5a98a30cdb25417d
MaleeshaSulakshana <mpmsulakshana98@gmail.com>
2023-05-13 21:56:58 +0530

    added: some functionalies for mobile side and added auth user api for login

---
+++ Mobile/EpicAmbulance/app/src/main/java/com/example/epicambulance/Classes/JWTUtils.java
@@ -0,0 +1,2 @@
+package com.example.epicambulance.Classes;public class JWTUtils {
+}
---
+++ Mobile/EpicAmbulance/app/src/main/java/com/example/epicambulance/User/HospitalsActivity.java
@@ -0,0 +1,16 @@
+package com.example.epicambulance.User;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.epicambulance.R;
+
+public class HospitalsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hospitals);
+    }
+}
